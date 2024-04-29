4c347d86421379f111d0737a2c638404a014a2fc
Entekallis <mforilliere11@gmail.com>
2024-04-29 14:33:38 +0200

    amélioration intro-discussion-conclusion (EN COURS)

--- CODE/Resumax/body.py
+++ CODE/Resumax/body.py
@@ -6,11 +6,12 @@ import fitz
 def find_intro(doc:fitz.Document, page_num:int, blknum:int):
 	found_intro = False
 	finish_intro = False
-	do_body = False
 
 
+	newblk = blknum
+	newpage = page_num
 	intro = ""
-	corps = ""
+
 	for page in range(page_num, len(doc)):
 		for i in range(blknum, len(doc[page].get_text("blocks"))):
 			block = doc[page].get_text("blocks")[i]
@@ -20,73 +21,98 @@ def find_intro(doc:fitz.Document, page_num:int, blknum:int):
 					found_intro = True
 					intro += text
 			else : # on a trouvé le début de l'intro
-				if finish_intro == False:
+				if finish_intro == False :
 					#print("ALED\n" + text.lower())
 					if not re.match(r"\A( *)(?:([0-9]|\.)|i+)( *)(\n|-|\.)", text.lower()):
-						intro += "##################\n#BLOCK SEPARATION#\n##################\n" + text
+						intro += text
 					else:
 						finish_intro = True
-						corps += "##################\n#BLOCK SEPARATION#\n##################\n" + text
-				else :
-					corps += "##################\n#BLOCK SEPARATION#\n##################\n" + text
+						break
+		if finish_intro:
+			break
+		newblk += 1
+	newpage += 1
 
-	return intro, corps
+	if intro == "" :
+		intro = "N/A"
+		return intro, page_num, blknum
+
+	return intro, newpage, newblk
 
 
-def extract_discuss(body: str):
-	decompo = body.split("\n")
-	recompo = ""
-	discussed = ""
-
+def find_discuss(doc : fitz.Document, page_num:int, blknum:int):
 	found_discuss = False
-	for line in decompo:
-		if found_discuss:
-			decompo.remove(line)
-			discussed += line + "\n"
-		else:
-			if re.match(r"[0-9]*(?: *|)(?:- |)discussion(?:s|)", line.lower()):  # Regex pour Discussion
-				# print("Discussion found")
-				found_discuss = True
-			else:
-				recompo += line + "\n"
-	return discussed, recompo
+	finished_discuss = False
+
+	discussed = ""
+	newpage = page_num
+	newblk = blknum
+
+	for page in range(page_num, len(doc)-1):
+		for i in range(blknum, len(doc[page].get_text("blocks"))-1):
+			block = doc[page].get_text("blocks")[i]
+			text = block[4]
+			if not found_discuss:
+				if re.match(r"(?:[0-9]|\.)+(?: *|)(?:- |)discussion(?:s|)", text.lower()):  # Regex pour Discussion
+					found_discuss = True
+					discussed += text
+			else: #on a trouvé le début de la discussion
+				if finished_discuss == False :
+					if re.fullmatch(r"[0-9]*[ .\-)/]*conclusion(?:s|)", text.lower()): #On s'arrete quand on tombe sur la conclusion
+						finished_discuss = True
+						break
+					discussed += "###########BLOCK DELIMITER###########" + text
+						# print("Discussion found")
+		if not finished_discuss :
+			break
+		newblk += 1
+	newpage += 1
+
+	if discussed == "":
+		discussed = "N/A"
+		return discussed, page_num, blknum
+	return discussed, newpage, newblk
 
 
 
-def find_conclusion(discussion: str, body: str):
+def find_conclusion(doc:fitz.Document, page_num:int, blknum:int):
 	found_conclu = False
+	finished_conclu = False
 	conclu = ""
-	discu = ""
-	autre = ""
-	# si la discussion est vide, la conclusion se trouve dans le body
-	if discussion == "":
-		for element in body.split("\n"):
-			if found_conclu:
-				conclu += element + "\n"
-			else:
-				if re.fullmatch(r"[0-9]*[ .\-)/]*conclusion(?:s|)", element.lower()):
+	newpage = page_num
+	newblk = blknum
+
+	for page in range(page_num, len(doc)-1):
+		for i in range(blknum, len(doc[page].get_text("blocks"))-1):
+			block = doc[page].get_text("blocks")[i]
+			text = block[4]
+			if not found_conclu:
+				if re.match(r"(?:[0-9]|\.)+(?: *|)(?:- |)conclusion(?:s|)", text.lower()):  # Regex pour Discussion
 					found_conclu = True
-				else:
-					discu += element + "\n"
-		return (discussion != ""), autre, conclu
-	# sinon la conclusion est dans la discussion
-	else:
-		for element in discussion.split("\n"):
-			if found_conclu:
-				conclu += element + "\n"
-			else:
-				if re.fullmatch(r"[0-9]*[ .\-)/]*conclusion(?:s|)", element.lower()):
-					found_conclu = True
-				else:
-					discu += element + "\n"
-		return (discussion != ""), discu, conclu
+					conclu += text
+			else: #on a trouvé le début de la discussion
+				if finished_conclu == False :
+					if re.fullmatch(r"\Areferences(?: |\n|)+", text.lower()): #On s'arrete quand on tombe sur la conclusion
+						finished_conclu = True
+						break
+					conclu += "###########BLOCK DELIMITER###########" + text
+
+		if not finished_conclu :
+			break
+		newblk += 1
+	newpage += 1
+
+	if conclu == "":
+		conclu = "N/A"
+		return conclu, page_num, blknum
+	return conclu, newpage, newblk
+
+
 
 
 if __name__ == "__main__":
-	intro, body = find_intro(fitz.open("../ressources/On_the_Morality_of_Artificial_Intelligence.pdf"), 0,0)
+	intro = find_conclusion(fitz.open("../ressources/surveyTermExtraction.pdf"), 0,0)[0]
 	print("-----------INTRO-------------")
 	print(intro)
 	print("-----------BODY--------------")
-	print(body)
 
-#TODO : sur On the morality machinmachin y'a pas d'intro :/
